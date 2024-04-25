e157b486dbecc724242f0599d46abc8e587e3fdb
Navin Joshi <naveenj.iteg2022@ssism.org>
2024-04-25 13:20:45 +0530

    build logic

--- BasicProject/src/App.jsx
+++ BasicProject/src/App.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
 function App() {
-  return <h1>hello wold</h1>;
+  // let counter = 15;
+
+  let [counter, setCounter] = useState(15);
+  const addValue = () => {
+    if(counter <20)
+    setCounter(counter + 1);
+  };
+  let removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+    else{
+      alert("counter is 0")
+    }
+  };
+  return (
+    <>
+      <h1>hello wold</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={removeValue}>remove value</button>
+    </>
+  );
 }
 
 export default App;
