55b7bb1a3c55c1b8b62aee40424286fbf4d8e0d8
Navin Joshi <naveenj.iteg2022@ssism.org>
2024-05-10 15:11:07 +0530

    todo fix

--- ToDoApp/src/components/TodoItem.jsx
+++ ToDoApp/src/components/TodoItem.jsx
@@ -50,7 +50,7 @@ function TodoItem({ todo }) {
       >
         {isTodoEditable ? "📁" : "✏️"}
       </button>
-      {/* Delete Todo Button */}
+      {/* Delet Button */}
       <button
         className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
         onClick={() => deleteTodo(todo.id)}
